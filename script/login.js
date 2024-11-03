55cafd987e55ba85af0777c2f4653ce638c0f8b2
keenawa <keenywira@gmail.com>
2024-11-04 01:19:18 +0800

    halaman login

---
+++ script/login.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $('#loginForm').submit(function (e) {
+        e.preventDefault();
+
+        var username = $('#name').val();
+        var password = $('#pass').val();
+
+        const takeUsername = localStorage.getItem('username');
+        const takePassword = localStorage.getItem('password');
+
+        if (password.length < 6) {
+            alert('Password must be at least 6 characters!');
+            return;
+        }else if (username === takeUsername && password === takePassword) {
+            alert('Login Successful!');
+            window.location.href = 'sampleHome.html'
+        }else {
+            alert('Wrong Password or Username!');
+        }
+    });
+});
